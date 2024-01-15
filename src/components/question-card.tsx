70a571a9976c037ea39e37fe6328b95814a34c03
saadfrhan <saadfarhan347@gmail.com>
2024-01-16 03:38:33 +0500

    update

--- src/components/question-card.tsx
+++ src/components/question-card.tsx
@@ -8,30 +8,38 @@ export default function QuestionCard({
   answers,
   callback,
   question,
-	nextQuestion,
+  nextQuestion,
 }: QuestionCardProps) {
-	const [selectedAnswer, setSelectedAnswer] = useState('');
+  const [selectedAnswer, setSelectedAnswer] = useState("");
 
   return (
     <div className="space-y-2">
       <p dangerouslySetInnerHTML={{ __html: question }} />
       <div className="flex flex-col gap-2">
-			<RadioGroup>
-        {answers.map((answer, index) => (
-          <div className="flex items-center space-x-2" key={index}>
-					<RadioGroupItem onClick={() => setSelectedAnswer(answer)} value={answer} id={answer} />
-					<Label htmlFor={answer}>{answer}</Label>
-				</div>
-        ))}
-				</RadioGroup>
+        <RadioGroup>
+          {answers.map((answer, index) => (
+            <div className="flex items-center space-x-2" key={index}>
+              <RadioGroupItem
+                onClick={() => setSelectedAnswer(answer)}
+                value={answer}
+                id={answer}
+              />
+              <Label htmlFor={answer}>{answer}</Label>
+            </div>
+          ))}
+        </RadioGroup>
       </div>
-			<Button className="w-full" onClick={() => {
-				callback(selectedAnswer);
-				nextQuestion();
-			setSelectedAnswer('');
-			}} disabled={selectedAnswer ? false : true}>
-				Next
-			</Button>
+      <Button
+        className="w-full"
+        onClick={() => {
+          callback(selectedAnswer);
+          nextQuestion();
+          setSelectedAnswer("");
+        }}
+        disabled={selectedAnswer ? false : true}
+      >
+        Next
+      </Button>
     </div>
   );
 }
